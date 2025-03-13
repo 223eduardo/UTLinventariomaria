2cc6d9fb689ee0940fa49bd8ecfecf03a838c396
Yahir Abraham <yahir.abraham.h@gmail.com>
2025-03-12 20:25:35 -0600

    Update inventarioini.php

--- inventarioini.php
+++ inventarioini.php
@@ -14,8 +14,8 @@ echo '
 <!-- Sidebar -->
 <div class="w-1/5 bg-white shadow-md relative">
     <div class="p-4 flex items-center justify-center">
-        <img src="icons_login/Imagen1.png" class="w-8 h-8 mr-2">
-        <h1 class="text-xl font-bold text-blue-900">Almacén</h1>
+        <i class="fas fa-warehouse" style="color: #012b4b; font-size: 32px; margin-right: 8px;"></i>
+    <h1 style="color: #012b4b; font-size: 1.25rem; font-weight: bold;">Almacén</h1>
     </div>
     <nav class="mt-4">
         <ul>
