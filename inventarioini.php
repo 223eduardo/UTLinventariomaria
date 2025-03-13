1def7eb908b54b5469907f40793bcaa2460c8b0b
Yahir Abraham <yahir.abraham.h@gmail.com>
2025-03-12 19:22:17 -0600

    Update inventarioini.php

--- inventarioini.php
+++ inventarioini.php
@@ -32,7 +32,7 @@ echo '
                 <span class="text-gray-500 hover:text-blue-900">Préstamos</span>
             </li>
             <li class="flex items-center px-4 py-2 rounded-lg transition-all duration-300 hover:bg-blue-100">
-                <img src="icons_login/4.png" class="w-5 h-5 mr-2">
+                <img src="../icons_login/4.png" class="w-5 h-5 mr-2">
                 <span class="text-gray-500 hover:text-blue-900" onclick=(cam0())>Perfil</span>
             </li>
         </ul>
